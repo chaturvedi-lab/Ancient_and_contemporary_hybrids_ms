dcf39475861f4e843a0a545a5aa9e68cb7872eab
samridhichaturvedi <schaturvedi@aggiemail.usu.edu>
2019-10-08 13:41:06 -0400

    Create align_fastq_bwamem.sh

---
+++ alignment/align_fastq_bwamem.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# written by Samridhi Chaturvedi
+# this script parses through the current directoy and aligns all .fastq files to a reference (REF)
+# BWA MEM is the alignment algorithm used in this script
+
+module load bwa
+
+# change this path to your reference genome or pseudo-reference genome created in the first part of the lycaeides_GBS_pipeline cheat sheet
+genome ='/uufs/chpc.utah.edu/common/home/u6007910/projects/lyc_dubois/lycaeides_dubois/Alignments/fastqfiles/melissa_blue_21Nov2017_GLtS4.fasta
+
+for file in *.fastq;
+do
+
+bwa mem -t 12 -k 15 -r 1.3 -T 30 $genome $file > mem"."$file".".sam
+
+done
