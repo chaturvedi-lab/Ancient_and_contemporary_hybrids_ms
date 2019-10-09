746984c18e415f2b4908212080d5db87951835a5
Samridhi Chaturvedi <schaturvedi@aggiemail.usu.edu>
2019-10-08 22:37:13 -0400

    Update forkRunEntropy.pl

--- entropy/forkRunEntropy.pl
+++ entropy/forkRunEntropy.pl
@@ -12,17 +12,16 @@ my $max = shift(@ARGV); #get number of cores to use at one time
 my $pm = Parallel::ForkManager->new($max);
 
 #path to the genotype likelihood file
-my $in = "/uufs/chpc.utah.edu/common/home/u6007910/projects/lyc_dubois/entropy/subset_filtered_variantsLycaeides.gl";
+my $in = "examples.gl";
 #path to the starting value files
-my $qdir = "/uufs/chpc.utah.edu/common/home/u6007910/projects/lyc_dubois/entropy/startingvals/subset/";
+my $qdir = "./";
 
 foreach my $ch (1..3){
         CHAINS:
         foreach my $k (2..5){
                 $pm->start and next CHAINS; ##fork;
-                print "/uufs/chpc.utah.edu/common/home/u6000989/bin/entropy -i $in -l 8000 -b 5000 -t 3 -k $k -Q 0 -s 50 -q $qdir"."ldak$k.txt -o /scratch/general/lustre/entropy/ento_lyc_hybridsCh$ch"."K$k.hdf5 -w 0 -m 1\nmv /scratch/general/lustre/entropy/ento_lyc_hybridsCh$ch"."K$k.hdf5 /uufs/chpc.utah.edu/common/home/u6000989/projects/lyc_dubois/entropy/mcmc/";
-                #system"sleep 2\n";
-                system "/uufs/chpc.utah.edu/common/home/u6000989/bin/entropy -i $in -l 8000 -b 5000 -t 3 -k $k -Q 0 -s 50 -q $qdir"."ldak$k.txt -o /scratch/general/lustre/entropy/ento_lyc_hybridsCh$ch"."K$k.hdf5 -w 0 -m 1\nmv /scratch/general/lustre/entropy/ento_lyc_hybridsCh$ch"."K$k.hdf5 /uufs/chpc.utah.edu/common/home/u6000989/projects/lyc_dubois/entropy/mcmc/";
+                print "entropy -i $in -l 8000 -b 5000 -t 3 -k $k -Q 0 -s 50 -q $qdir"."ldak$k.txt -o ./ento_lyc_hybridsCh$ch"."K$k.hdf5 -w 0 -m 1";
+                system "entropy -i $in -l 8000 -b 5000 -t 3 -k $k -Q 0 -s 50 -q $qdir"."ldak$k.txt -o ./ento_lyc_hybridsCh$ch"."K$k.hdf5 -w 0 -m 1";
                         $pm->finish; ## exit the child process
                 }
         }      
