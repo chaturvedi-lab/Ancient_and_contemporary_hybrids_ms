6a9f7f8fc74b818e3a49e58411226df81d72e36c
Samridhi Chaturvedi <schaturvedi@aggiemail.usu.edu>
2019-10-08 22:22:12 -0400

    Update forkPopanc.pl

--- popanc/forkPopanc.pl
+++ popanc/forkPopanc.pl
@@ -9,34 +9,22 @@ use Parallel::ForkManager;
 my $max = shift(@ARGV); #get number of cores to use at one time
 my $pm = Parallel::ForkManager->new($max);
 
-my $indir = "/uufs/chpc.utah.edu/common/home/u6007910/projects/lyc_dubois/popanc/infiles/";
-my $outdir = "/uufs/chpc.utah.edu/common/home/u6007910/projects/lyc_dubois/popanc/outfiles/";
+my $indir = "./";
+#modify this directory
+my $outdir = "./outfiles/";
 my @w = (3,5,7);
-my @melissa = ("all","submore","sub");
-my @idas = ("all","bld_frc","notpure","pure");
-my @hpops = ("BLD","BNP", "PSP");
-#my @hpops = ("BIC","BTB","DBS","PIN","PSP","RDL","RNV");
-
-
-#module load gcc
-#module load gsl
-#module load hdf5
+my @hpops = ("BLD","BIC");
 
 CHAINS:
 foreach my $w (@w){
         $pm->start and next CHAINS;
 #CHAINS:
         foreach my $pop (@hpops){
-                foreach my $mel (@melissa){
-                        foreach my $idas (@idas){
-                                #$pm->start and next CHAINS; ##fork;
-                                my $base = $mel . '_' . $idas . '_' . $pop . '_' . $w;
-                                print "/uufs/chpc.utah.edu/common/home/u6000989/bin/popanc -o $outdir"."out_$base.hdf5 -n $w -d 15000 -m 10000 -b 5000 -t 10 -s 1 -q 1 -z 1 infiles/comb_idas_$idas.txt infiles/comb_melissa_$mel.txt infiles/lyc_genoest_$pop.txt";
-                                #system"sleep 2\n";
-                                system "/uufs/chpc.utah.edu/common/home/u6000989/bin/popanc -o $outdir"."out_$base.hdf5 -n $w -d 15000 -m 10000 -b 5000 -t 10 -s 1 -q 1 -z 1 infiles/comb_idas_$idas.txt infiles/comb_melissa_$mel.txt infiles/lyc_genoest_$pop.txt";
+                my $base = $mel . '_' . $idas . '_' . $pop . '_' . $w;
+                print "/uufs/chpc.utah.edu/common/home/u6000989/bin/popanc -o $outdir"."out_$base.hdf5 -n $w -d 15000 -m 10000 -b 5000 -t 10 -s 1 -q 1 -z 1 ./idas.txt ./melissa.txt ./lyc_genoest_$pop.txt";
+                #system"sleep 2\n";
+                system "/uufs/chpc.utah.edu/common/home/u6000989/bin/popanc -o $outdir"."out_$base.hdf5 -n $w -d 15000 -m 10000 -b 5000 -t 10 -s 1 -q 1 -z 1 ./idas.txt ./melissa.txt ./lyc_genoest_$pop.txt"";
                         $pm->finish; ##exit the child process
-        }
-        }
         }
 }
 
