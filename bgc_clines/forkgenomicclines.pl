5a67855b63c91be2723e1b15e34fbd33ff30d1c7
Samridhi Chaturvedi <schaturvedi@aggiemail.usu.edu>
2019-10-08 22:01:04 -0400

    Update forkgenomicclines.pl

--- bgc_clines/forkgenomicclines.pl
+++ bgc_clines/forkgenomicclines.pl
@@ -9,21 +9,17 @@ use Parallel::ForkManager;
 my $max = shift(@ARGV); #get number of cores to use at one time
 my $pm = Parallel::ForkManager->new($max);
 
-#directory for in files
-my $dir = '/uufs/chpc.utah.edu/common/home/u6007910/projects/lyc_dubois/clines/';
-#directory for output files
-my $odir = "/uufs/chpc.utah.edu/common/home/u6007910/projects/lyc_dubois/clines/outfiles/";
+#specify directory for in files
+my $dir = './';
+#specify directory for output files
+my $odir = "./outdir";
 
 CHAINS:
 foreach my $j (0..4){
                  $pm->start and next CHAINS; ##fork;
-				print "/uufs/chpc.utah.edu/common/home/u6000989/bin/bgc -a $dir"."idas_p01_pct1.gl -b $dir"."melissa_p02_pct1.gl -h $dir"."admixed_dbs_pct1.gl -F $odir"."bgcout_pct1_$j -O 0 -x 25000 -n 5000 -t 5 -p 1 -q 1 -N 1 -m 0 -d 1 -s 1 -I 1 -T 0";
-                                print "/uufs/chpc.utah.edu/common/home/u6000989/bin/bgc -a $dir"."idas_p01_pct2.gl -b $dir"."melissa_p02_pct2.gl -h $dir"."admixed_dbs_pct2.gl -F $odir"."bgcout_pct2_$j -O 0 -x 25000 -n 5000 -t 5 -p 1 -q 1 -N 1 -m 0 -d 1 -s 1 -I 1 -T 0";
-                                print "/uufs/chpc.utah.edu/common/home/u6000989/bin/bgc -a $dir"."idas_p01_pct3.gl -b $dir"."melissa_p02_pct3.gl -h $dir"."admixed_dbs_pct3.gl -F $odir"."bgcout_pct3_$j -O 0 -x 25000 -n 5000 -t 5 -p 1 -q 1 -N 1 -m 0 -d 1 -s 1 -I 1 -T 0";
+				print "/uufs/chpc.utah.edu/common/home/u6000989/bin/bgc -a $dir"."idas.gl -b $dir"."melissa.gl -h $dir"."admixed.gl -F $odir"."bgcout_$j -O 0 -x 25000 -n 5000 -t 5 -p 1 -q 1 -N 1 -m 0 -d 1 -s 1 -I 1 -T 0";
 				system"sleep 3\n";
-                          	system "/uufs/chpc.utah.edu/common/home/u6000989/bin/bgc -a $dir"."idas_p01_pct1.gl -b $dir"."melissa_p02_pct1.gl -h $dir"."admixed_dbs_pct1.gl -F $odir"."bgcout_pct1_$j -O 0 -x 25000 -n 5000 -t 5 -p 1 -q 1 -N 1 -m 0 -d 1 -s 1 -I 1 -T 0";
-				system "/uufs/chpc.utah.edu/common/home/u6000989/bin/bgc -a $dir"."idas_p01_pct2.gl -b $dir"."melissa_p02_pct2.gl -h $dir"."admixed_dbs_pct2.gl -F $odir"."bgcout_pct2_$j -O 0 -x 25000 -n 5000 -t 5 -p 1 -q 1 -N 1 -m 0 -d 1 -s 1 -I 1 -T 0";
-				system "/uufs/chpc.utah.edu/common/home/u6000989/bin/bgc -a $dir"."idas_p01_pct3.gl -b $dir"."melissa_p02_pct3.gl -h $dir"."admixed_dbs_pct3.gl -F $odir"."bgcout_pct3_$j -O 0 -x 25000 -n 5000 -t 5 -p 1 -q 1 -N 1 -m 0 -d 1 -s 1 -I 1 -T 0";
+                          	system "/uufs/chpc.utah.edu/common/home/u6000989/bin/bgc -a $dir"."idas.gl -b $dir"."melissa.gl -h $dir"."admixed.gl -F $odir"."bgcout_$j -O 0 -x 25000 -n 5000 -t 5 -p 1 -q 1 -N 1 -m 0 -d 1 -s 1 -I 1 -T 0";
                                  }
 
 $pm->wait_all_children;
